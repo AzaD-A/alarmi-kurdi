ed466adfe80083e3e5329675dfa8088a789ffa15
azad ahmad mhamad <58448316+AzaD-A@users.noreply.github.com>
2019-12-20 09:11:56 +0000

    V 1.0

---
+++ alarmLINUX.py
@@ -0,0 +1,472 @@
+# created by azad
+# follow me on IG @p8z4
+# rez u xoshawistim bo arsalan parosh haya
+from playsound import playsound, PlaysoundException
+from time import sleep
+import os
+import sys
+
+def azad():
+    os.system('clear')
+    print('''
+    
+             __                     _       __                  ___ 
+      ____ _/ /___ __________ ___  (_)     / /____  ___________/ (_)
+     / __ `/ / __ `/ ___/ __ `__ \/ /_____/ //_/ / / / ___/ __  / / 
+    / /_/ / / /_/ / /  / / / / / / /_____/ ,< / /_/ / /  / /_/ / /  
+    \__,_/_/\__,_/_/  /_/ /_/ /_/_/     /_/|_|\__,_/_/   \__,_/_/   
+    with azad V 1.0 Emm Bayanyan Ba Alarm Hallsa                                                                
+           [ Kat Dananawa u jaras leyan ]
+    
+    [1]- kat danan ba xulak(daqa)
+    
+    [2]- kat danan ba katzhmer 
+    
+    ''')
+    azadd = input(' [+] kamayan  : ')
+    if azadd == '1':
+        xulak()
+    elif azadd == '2':
+        kat()
+    else:
+        print(' [!] Tkaya 1 yan 2 Halbzhera [!]')
+        
+def xulak():
+    os.system('clear')
+    print('''
+    
+    ███╗ ██╗███╗    ██╗  ██╗██╗   ██╗██╗      █████╗ ██╗  ██╗
+    ██╔╝███║╚██║    ╚██╗██╔╝██║   ██║██║     ██╔══██╗██║ ██╔╝
+    ██║ ╚██║ ██║     ╚███╔╝ ██║   ██║██║     ███████║█████╔╝ 
+    ██║  ██║ ██║     ██╔██╗ ██║   ██║██║     ██╔══██║██╔═██╗ 
+    ███╗ ██║███║    ██╔╝ ██╗╚██████╔╝███████╗██║  ██║██║  ██╗
+    ╚══╝ ╚═╝╚══╝    ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝
+     with azad                                                        
+
+    ''')
+    azo = input('[-] Chand Xulak da Aneytawa(1-60) : ' )
+    
+    if azo == '1':
+        a = int(azo)
+    elif azo == '2':
+        b = int(azo)
+    elif azo == '3':
+        c = int(azo)
+    elif azo == '4':
+        d = int(azo)
+    elif azo == '5':
+        e = int(azo)
+    elif azo == '6':
+        f = int(azo)
+    elif azo == '7':
+        g = int(azo)
+    elif azo == '8':
+        h = int(azo)
+    elif azo == '9':
+        i = int(azo)
+    elif azo == '10':
+        j = int(azo)
+    elif azo == '11':
+        k = int(azo)
+    elif azo == '12':
+        l = int(azo)
+    elif azo == '13':
+        m = int(azo)
+    elif azo == '14':
+        n = int(azo)
+    elif azo == '15':
+        o = int(azo)
+    elif azo == '16':
+        p = int(azo)
+    elif azo == '17':
+        q = int(azo)
+    elif azo == '18':
+        r = int(azo)
+    elif azo == '19':
+        s = int(azo)
+    elif azo == '20':
+        t = int(azo)
+    elif azo == '21':
+        u = int(azo)
+    elif azo == '22':
+        v = int(azo)
+    elif azo == '23':
+        w = int(azo)
+    elif azo == '24':
+        x = int(azo)
+    elif azo == '25':
+        y = int(azo)
+    elif azo == '26':
+        z = int(azo)
+    elif azo == '27':
+        a1 = int(azo)
+    elif azo == '28':
+        a2 = int(azo)
+    elif azo == '29':
+        a3 = int(azo)
+    elif azo == '30':
+        a4 = int(azo)
+    elif azo == '31':
+        a5 = int(azo)
+    elif azo == '32':
+        a6 = int(azo)
+    elif azo == '33':
+        a7 = int(azo)
+    elif azo == '34':
+        a8 = int(azo)
+    elif azo == '35':
+        a9 = int(azo)
+    elif azo == '36':
+        a10 = int(azo)
+    elif azo == '37':
+        a11 = int(azo)
+    elif azo == '38':
+        a12 = int(azo)
+    elif azo == '39':
+        a13 = int(azo)
+    elif azo == '40':
+        a14 = int(azo)
+    elif azo == '41':
+        a15 = int(azo)
+    elif azo == '42':
+        a16 = int(azo)
+    elif azo == '43':
+        a17 = int(azo)
+    elif azo == '44':
+        a18 = int(azo)
+    elif azo == '45':
+        a19 = int(azo)
+    elif azo == '46':
+        a20 = int(azo)
+    elif azo == '47':
+        a21 = int(azo)
+    elif azo == '48':
+        a22 = int(azo)
+    elif azo == '49':
+        a23 = int(azo)
+    elif azo == '50':
+        a24 = int(azo)
+    elif azo == '51':
+        a25 = int(azo)
+    elif azo == '52':
+        a26 = int(azo)
+    elif azo == '53':
+        a27 = int(azo)
+    elif azo == '54':
+        a28 = int(azo)
+    elif azo == '55':
+        a29 = int(azo)
+    elif azo == '56':
+        a30 = int(azo)
+    elif azo == '57':
+        a31 = int(azo)
+    elif azo == '58':
+        a32 = int(azo)
+    elif azo == '59': 
+        a33 = int(azo)
+    elif azo == '60':
+        a34 = int(azo)
+    else:
+        print('[!] Tkaya 1 ta 60 Halbzhera [!] ')
+    try:
+        if azo == '1':
+            print(f"[-] dway {a} xulak jaras le adat " )
+            sleep(a * 60)
+        elif azo == '2':
+            print(f"[-] dway {b} xulak jaras le adat ." )
+            sleep(b * 60)
+        elif azo == '3':
+            print(f"[-] dway {c} xulak jaras le adat ." )
+            sleep(c * 60)
+        elif azo == '4':
+            print(f"[-] dway {d} xulak jaras le adat ." )
+            sleep(d * 60)
+        elif azo == '5':
+            print(f"[-] dway {e} xulak jaras le adat ." )
+            sleep(e * 60)
+        elif azo == '6':
+            print(f"[-] dway {f} xulak jaras le adat ." )
+            sleep(f * 60)
+        elif azo == '7':
+            print(f"[-] dway {g} xulak jaras le adat ." )
+            sleep(g * 60)
+        elif azo == '8':
+            print(f"[-] dway {h} xulak jaras le adat ." )
+            sleep(h * 60)
+        elif azo == '9':
+            print(f"[-] dway {i} xulak jaras le adat ." )
+            sleep(i * 60)
+        elif azo == '10':
+            print(f"[-] dway {j} xulak jaras le adat ." )
+            sleep(j * 60)
+        elif azo == '11':
+            print(f"[-] dway {k} xulak jaras le adat ." )
+            sleep(k * 60)
+        elif azo == '12':
+            print(f"[-] dway {l} xulak jaras le adat ." )
+            sleep(l * 60)
+        elif azo == '13':
+            print(f"[-] dway {m} xulak jaras le adat ." )
+            sleep(m * 60)
+        elif azo == '14':
+            print(f"[-] dway {n} xulak jaras le adat ." )
+            sleep(n * 60)
+        elif azo == '15':
+            print(f"[-] dway {o} xulak jaras le adat ." )
+            sleep(o * 60)        
+        elif azo == '16':
+            print(f"[-] dway {p} xulak jaras le adat ." )
+            sleep(p * 60)
+        elif azo == '17':
+            print(f"[-] dway {q} xulak jaras le adat ." )
+            sleep(q * 60)
+        elif azo == '18':
+            print(f"[-] dway {r} xulak jaras le adat ." )
+            sleep(r * 60)
+        elif azo == '19':
+            print(f"[-] dway {s} xulak jaras le adat ." )
+            sleep(s * 60)
+        elif azo == '20':
+            print(f"[-] dway {t} xulak jaras le adat ." )
+            sleep(t * 60)
+        elif azo == '21':
+            print(f"[-] dway {u} xulak jaras le adat ." )
+            sleep(u * 60)
+        elif azo == '22':
+            print(f"[-] dway {v} xulak jaras le adat ." )
+            sleep(v * 60)
+        elif azo == '23':
+            print(f"[-] dway {w} xulak jaras le adat ." )
+            sleep(w * 60)
+        elif azo == '24':
+            print(f"[-] dway {x} xulak jaras le adat ." )
+            sleep(x * 60)
+        elif azo == '25':
+            print(f"[-] dway {y} xulak jaras le adat ." )
+            sleep(y * 60)
+        elif azo == '26':
+            print(f"[-] dway {z} xulak jaras le adat ." )
+            sleep(z * 60)
+        elif azo == '27':
+            print(f"[-] dway {a1} xulak jaras le adat ." )
+            sleep(a1 * 60)
+        elif azo == '28':
+            print(f"[-] dway {a2} xulak jaras le adat ." )
+            sleep(a2 * 60)
+        elif azo == '29':
+            print(f"[-] dway {a3} xulak jaras le adat ." )
+            sleep(a3 * 60)
+        elif azo == '30':
+            print(f"[-] dway {a4} xulak jaras le adat ." )
+            sleep(a4 * 60)
+        elif azo == '31':
+            print(f"[-] dway {a5} xulak jaras le adat ." )
+            sleep(a5 * 60)
+        elif azo == '32':
+            print(f"[-] dway {a6} xulak jaras le adat ." )
+            sleep(a6 * 60)
+        elif azo == '33':
+            print(f"[-] dway {a7} xulak jaras le adat ." )
+            sleep(a7 * 60)
+        elif azo == '34':
+            print(f"[-] dway {a8} xulak jaras le adat ." )
+            sleep(a8 * 60)
+        elif azo == '35':
+            print(f"[-] dway {a9} xulak jaras le adat ." )
+            sleep(a9 * 60)
+        elif azo == '36':
+            print(f"[-] dway {a10} xulak jaras le adat ." )
+            sleep(a10 * 60)
+        elif azo == '37':
+            print(f"[-] dway {a11} xulak jaras le adat ." )
+            sleep(a11 * 60)
+        elif azo == '38':
+            print(f"[-] dway {a12} xulak jaras le adat ." )
+            sleep(a12 * 60)
+        elif azo == '39':
+            print(f"[-] dway {a13} xulak jaras le adat ." )
+            sleep(a13 * 60)
+        elif azo == '40':
+            print(f"[-] dway {a14} xulak jaras le adat ." )
+            sleep(a14 * 60)
+        elif azo == '41':
+            print(f"[-] dway {a15} xulak jaras le adat ." )
+            sleep(a15 * 60)
+        elif azo == '42':
+            print(f"[-] dway {a16} xulak jaras le adat ." )
+            sleep(a16 * 60)
+        elif azo == '43':
+            print(f"[-] dway {a17} xulak jaras le adat ." )
+            sleep(a17 * 60)
+        elif azo == '44':
+            print(f"[-] dway {a18} xulak jaras le adat ." )
+            sleep(a18 * 60)
+        elif azo == '45':
+            print(f"[-] dway {a19} xulak jaras le adat ." )
+            sleep(a19 * 60)
+        elif azo == '46':
+            print(f"[-] dway {a20} xulak jaras le adat ." )
+            sleep(a20 * 60)
+        elif azo == '47':
+            print(f"[-] dway {a21} xulak jaras le adat ." )
+            sleep(a21 * 60)
+        elif azo == '48':
+            print(f"[-] dway {a22} xulak jaras le adat ." )
+            sleep(a22 * 60)
+        elif azo == '49':
+            print(f"[-] dway {a23} xulak jaras le adat ." )
+            sleep(a23 * 60)
+        elif azo == '50':
+            print(f"[-] dway {a24} xulak jaras le adat ." )
+            sleep(a24 * 60)
+        elif azo == '51':
+            print(f"[-] dway {a25} xulak jaras le adat ." )
+            sleep(a25 * 60)
+        elif azo == '52':
+            print(f"[-] dway {a26} xulak jaras le adat ." )
+            sleep(a26 * 60)
+        elif azo == '53':
+            print(f"[-] dway {a27} xulak jaras le adat ." )
+            sleep(a27 * 60)
+        elif azo == '54':
+            print(f"[-] dway {a28} xulak jaras le adat ." )
+            sleep(a28 * 60)
+        elif azo == '55':
+            print(f"[-] dway {a29} xulak jaras le adat ." )
+            sleep(a29 * 60)
+        elif azo == '56':
+            print(f"[-] dway {a30} xulak jaras le adat ." )
+            sleep(a30 * 60)
+        elif azo == '57':
+            print(f"[-] dway {a31} xulak jaras le adat ." )
+            sleep(a31 * 60)
+        elif azo == '58':
+            print(f"[-] dway {a32} xulak jaras le adat ." )
+            sleep(a32 * 60)
+        elif azo == '59':
+            print(f"[-] dway {a33} xulak jaras le adat ." )
+            sleep(a33 * 60)
+        elif azo == '60':
+            print(f"[-] dway {a34} xulak jaras le adat ." )
+            sleep(a34 * 60)
+        
+        else:
+            print('[+] hasta la xaw -_-')
+        print('[+] hasta la xaw dllakam la qabr bm -_-')
+        for i in range(10):
+            if azo < str(10):
+                    playsound(r'hasta.mp3')
+
+    except PlaysoundException as pe:
+        print("error :  musicaka bar das nia ")
+        sys.exit(1)
+    except KeyboardInterrupt:
+        print("[+] supas bo bakar henani [+]")
+        sys.exit(1)
+def kat():
+        os.system('clear')
+        print('''
+                
+            ███╗██████╗ ███╗    ██╗  ██╗ █████╗ ████████╗
+            ██╔╝╚════██╗╚██║    ██║ ██╔╝██╔══██╗╚══██╔══╝
+            ██║  █████╔╝ ██║    █████╔╝ ███████║   ██║   
+            ██║ ██╔═══╝  ██║    ██╔═██╗ ██╔══██║   ██║   
+            ███╗███████╗███║    ██║  ██╗██║  ██║   ██║   
+            ╚══╝╚══════╝╚══╝    ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   
+             with azad                                             
+
+        ''')
+        avo = input('[+] chand Katzhmer da Aneytawa(1-15): ')
+        if avo == '1':
+            aa = int(avo)
+        elif avo == '2':
+            bb = int(avo)
+        elif avo == '3':
+            cc = int(avo)
+        elif avo == '4':
+            dd = int(avo)
+        elif avo == '5':
+            ee = int(avo)
+        elif avo == '6':
+            ff = int(avo)
+        elif avo == '7':
+            gg = int(avo)
+        elif avo == '8':
+            hh = int(avo)
+        elif avo == '9':
+            ii = int(avo)
+        elif avo == '10':
+            jj = int(avo)
+        elif avo == '11':
+            kk = int(avo)
+        elif avo == '12':
+            ll = int(avo)
+        elif avo == '13':
+            mm = int(avo)
+        elif avo == '14':
+            nn = int(avo)
+        elif avo == '15':
+            oo = int(avo)
+        else:
+            print('[!] Tkaya 1 ta 60 Halbzhera [!] ')
+        try:
+            if avo == '1':
+                print(f"[-] dway {aa} katzhmer jaras le adat " )
+                sleep(aa * 3600)
+            elif avo == '2':
+                print(f"[-] dway {bb} katzhmer jaras le adat ." )
+                sleep(bb * 3600)
+            elif azo == '3':
+                print(f"[-] dway {cc} katzhmer jaras le adat ." )
+                sleep(cc * 3600)
+            elif azo == '4':
+                print(f"[-] dway {dd} katzhmer jaras le adat ." )
+                sleep(dd * 3600)
+            elif azo == '5':
+                print(f"[-] dway {ee} katzhmer jaras le adat ." )
+                sleep(ee * 3600)
+            elif azo == '6':
+                print(f"[-] dway {ff} katzhmer jaras le adat ." )
+                sleep(ff * 3600)
+            elif azo == '7':
+                print(f"[-] dway {gg} katzhmer jaras le adat ." )
+                sleep(gg * 3600)
+            elif azo == '8':
+                print(f"[-] dway {hh} katzhmer jaras le adat ." )
+                sleep(hh * 3600)
+            elif azo == '9':
+                print(f"[-] dway {ii} katzhmer jaras le adat ." )
+                sleep(ii * 3600)
+            elif azo == '10':
+                print(f"[-] dway {jj} katzhmer jaras le adat ." )
+                sleep(jj * 3600)
+            elif azo == '11':
+                print(f"[-] dway {kk} katzhmer jaras le adat ." )
+                sleep(kk * 3600)
+            elif azo == '12':
+                print(f"[-] dway {ll} katzhmer jaras le adat ." )
+                sleep(ll * 3600)
+            elif azo == '13':
+                print(f"[-] dway {mm} katzhmer jaras le adat ." )
+                sleep(mm * 3600)
+            elif azo == '14':
+                print(f"[-] dway {nn} katzhmer jaras le adat ." )
+                sleep(nn * 3600)
+            elif azo == '15':
+                print(f"[-] dway {oo} katzhmer jaras le adat ." )
+                sleep(oo * 3600)
+            else:
+                print('[+] hasta la xaw -_-')
+            print('[+] hasta la xaw dllakam la qabr bm -_-')
+            for i in range(10):
+                if azo < str(10):
+                        playsound(r'hasta.mp3')
+        except PlaysoundException as pe:
+            print("Error: musicaka bar das nia ")
+            sys.exit(1)
+        except KeyboardInterrupt:
+            print(" supas bo bakar henani ")
+            sys.exit(1)
+            
+if __name__ == "__main__":
+    azad()
